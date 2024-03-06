2a2d3c43c25a69a64766e284edfef349e6846f3e
Simon Kennedy <sffjunkie+code@gmail.com>
2024-03-06 14:01:55 +0000

    Update test to use correct function

--- tests/integration/test_cache.py
+++ tests/integration/test_cache.py
@@ -1,10 +1,10 @@
 import os
 
 import pytest
-from ttsystemd.systemd.cache import load_systemd_interface_definition
+from ttsystemd.systemd.cache import systemd_load_interface_definition
 
 
 @pytest.mark.xfail("VSCODE_PID" in os.environ, reason="Fails under vscode")
-def test_load_systemd_interface_file():
-    interface = load_systemd_interface_definition("mount")
+def test_systemd_load_interface_definition():
+    interface = systemd_load_interface_definition("mount")
     assert interface is not None
